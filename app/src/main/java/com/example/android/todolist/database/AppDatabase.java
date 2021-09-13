4180591ad75eb8a6a56ce7649ef3e0a54a0a82a7
aditya <aditya003.ay@gmail.com>
2021-09-13 22:28:17 +0530

    Date TypeConvertors added

--- app/src/main/java/com/example/android/todolist/database/AppDatabase.java
+++ app/src/main/java/com/example/android/todolist/database/AppDatabase.java
@@ -6,10 +6,11 @@ import android.util.Log;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-
+import androidx.room.TypeConverters;
 
 
 @Database(entities = {TaskEntry.class}, version = 1 , exportSchema = false)
+@TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final  String LOG_TAG = AppDatabase.class.getSimpleName();
